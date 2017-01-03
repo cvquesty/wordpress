1f1f9129dc58665632cc0c902efcb07c820bc845
Jerald Sheets <jsheetsjr@paypal.com>
2017-01-03 10:34:16 -0700

    Adding extraction routine for wordpress archive

---
+++ manifests/extract.pp
@@ -0,0 +1,17 @@
+# Manifest to extract wordpress tarball from /var/tmp to
+# required vhost docroot
+#
+# === Parameters:
+#
+# [*version*] Version of Wordpress to extract
+# [*docroot*] Destination Document Root for Wordpress
+define wordpress::extract (
+  Number $version,
+  String $docroot,
+) {
+
+  exec { 'extractit':
+    onlyif  => "/bin/test -f /var/tmp/wordpress-${version}.tar.gz",
+    command => "/usr/bin/tar -zxvf /var/tmp/wordpress-${version}.tar.gz --strip 1 -C /var/www/html/${docroot}",
+  }
+}
