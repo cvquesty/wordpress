7f25779656fb7a6f50bcad187b4d92a2fc3f6a6c
Jerald Sheets <jsheetsjr@paypal.com>
2017-01-03 10:15:37 -0700

    moved download target to /var/tmp to ease addition of other OSes to the targets.

--- manifests/fetch.pp
+++ manifests/fetch.pp
@@ -3,18 +3,15 @@
 # === Parameters:
 #
 # [*version*] Version number of Wordpress to download
-# [*site*] Directory under /var/www/html to place retrieved archive
-# -- usually corresponds to "servername" directive from Vhost entry
 #
 define wordpress::fetch (
   Number $version,
-  String $site,
 ) {
 
   # Check that the archive actually exists
   exec { 'isitthere':
     onlyif  => "/usr/bin/wget --spider https://wordpress.org/wordpress-${version}.tar.gz",
-    command => "/usr/bin/wget https://wordpress.org/wordpress-${version}.tar.gz -P /var/www/html/${site}/wordpress-${version}.tar.gz",
+    command => "/usr/bin/wget https://wordpress.org/wordpress-${version}.tar.gz -P /var/tmp/wordpress-${version}.tar.gz",
   }
 
 }
