ef84f36d1be96c419006a60f4d6598874726144e
Jerald Sheets <jsheetsjr@paypal.com>
2017-01-04 07:39:46 -0700

    adding more conditionals to keep everything tight.

--- manifests/fetch.pp
+++ manifests/fetch.pp
@@ -3,13 +3,16 @@
 # === Parameters:
 #
 # [*version*] Version number of Wordpress to download
+# [*docroot*] Destination Document Root for Wordpress
 #
 define wordpress::fetch (
   $version,
+  $docroot,
 ) {
 
   # Check that the archive actually exists
   exec { 'getit':
+    unless  => "/bin/test -f /var/www/html/${docroot}/wp-comments-post.php",
     command => "/usr/bin/wget https://wordpress.org/wordpress-${version}.tar.gz -P /var/tmp",
   }
 
