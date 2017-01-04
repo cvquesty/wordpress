049a5e55ecde6a2fd8d906ae757bfacfbcc37381
Jerald Sheets <jsheetsjr@paypal.com>
2017-01-04 09:23:02 -0700

    adding in wp-config parameters to try and drop a wp-config file properly formatted.

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,16 +2,32 @@
 # wp-config.php as needed.
 #
 # === Parameters
-# [*wpversion*] Version of Wordpress to extract
-# [*sysdocroot*] Destination Document Root for Wordpress
+# [*wpversion*]       Version of Wordpress to extract
+# [*sysdocroot*]      Destination Document Root for Wordpress
+# [*db_name*]         Name of MySQL Database to use
+# [*db_user*]         DB User to use as Admin on this site
+# [*db_password*]     DB User Password to use
+# [*db_host*]         Node on which the database resides
+# [*wp_table_prefix*] Prefix, if any, you want prepended to table
 #
 class wordpress::config (
 
-  $wpversion  = $::wordpress::params::version,
-  $sysdocroot = $::wordpress::params::docroot,
+  $wpversion       = $::wordpress::params::version,
+  $sysdocroot      = $::wordpress::params::docroot,
+  $db_name         = $::wordpress::params::dbname,
+  $db_user         = $::wordpress::params::dbuser,
+  $db_password     = $::wordpress::params::dbpass,
+  $db_host         = $::wordpress::params::dbhost,
+  $wp_table_prefix = $::wordpress::params::tablepre,
 
 ) inherits wordpress::params {
 
-
+  file { "/var/www/html/${sysdocroot}/wp-config.php":
+    ensure  => 'present',
+    owner   => 'apache',
+    group   => 'apache',
+    mode    => '0644',
+    content => template('wordpress/wp-config_template.erb'),
+  }
 
 }
